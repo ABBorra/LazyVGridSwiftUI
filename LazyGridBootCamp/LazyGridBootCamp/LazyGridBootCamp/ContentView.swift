1741bb28bbf953599fac4c322cd28ab2fa9db8b5
AB_Borra <116728482+ABBorra@users.noreply.github.com>
2023-05-23 03:27:48 +0800

    Add files via upload

---
+++ LazyGridBootCamp/LazyGridBootCamp/LazyGridBootCamp/ContentView.swift
@@ -0,0 +1,78 @@
+//
+//  ContentView.swift
+//  LazyGridBootCamp
+//
+//  Created by AB_Borra on 5/23/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    let coloum: [GridItem] = [
+        GridItem(.flexible(), spacing: 6, alignment: nil),
+        
+        GridItem(.flexible(), spacing: 6, alignment: nil),
+        
+        GridItem(.flexible(), spacing: 6, alignment: nil),
+    ]
+    var body: some View {
+        
+        ScrollView {
+            
+            Text("LazyVGrid")
+                .font(Font.largeTitle)
+            LazyVGrid(
+                
+                columns: coloum,
+                alignment: .center,
+                spacing: 6, pinnedViews: [], content: {
+                    
+                    Section(header: Text("Section: 1")
+                        .foregroundColor(Color.white)
+                            
+                        .font(.title)
+                        .frame(maxWidth: .infinity, alignment: .leading)
+                        .background(Color.blue)
+                        .cornerRadius(5)
+                        .padding(2)
+                    )  {
+                        
+                        ForEach(0..<20) { index in
+                            Rectangle()
+                                .frame(height: 150)
+                                .foregroundColor(.red)
+                            
+                        }
+                    }
+                    
+                    Section(header: Text("Section: 2")
+                        .foregroundColor(Color.white)
+                            
+                        .font(.title)
+                        .frame(maxWidth: .infinity, alignment: .leading)
+                        .background(Color.yellow)
+                        .cornerRadius(5)
+                        .padding(2)
+                            
+                    )  {
+                        
+                        ForEach(0..<45) { index in
+                            Rectangle()
+                                .frame(height: 150)
+                                .foregroundColor(.green)
+                            
+                            
+                        }
+                    }
+                    
+                }).padding()
+            
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ LazyGridBootCamp/LazyGridBootCamp/LazyGridBootCamp/LazyGridBootCampApp.swift
@@ -0,0 +1,17 @@
+//
+//  LazyGridBootCampApp.swift
+//  LazyGridBootCamp
+//
+//  Created by AB_Borra on 5/23/23.
+//
+
+import SwiftUI
+
+@main
+struct LazyGridBootCampApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
